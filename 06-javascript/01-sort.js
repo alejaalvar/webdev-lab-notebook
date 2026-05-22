31c7fc77b4822b2713d38a3c2d1d01c62c6dad39
Alejandro Alvarado <alejalva@pdx.edu>
2026-05-22 11:46:10 -0700

    implement: numerical sorting

--- 06-javascript/01-sort.js
+++ 06-javascript/01-sort.js
@@ -1,34 +1,47 @@
 const pokemons = [
-  { id: 1, name: 'Bulbasaur', types: ['Grass', 'Poison'] },
-  { id: 2, name: 'Ivysaur', types: ['Grass', 'Poison'] },
-  { id: 3, name: 'Venusaur', types: ['Grass', 'Poison'] },
-  { id: 4, name: 'Charmander', types: ['Fire'] },
-  { id: 5, name: 'Charmeleon', types: ['Fire'] },
-  { id: 6, name: 'Charizard', types: ['Fire', 'Flying'] },
-  { id: 7, name: 'Squirtle', types: ['Water'] },
-  { id: 8, name: 'Wartortle', types: ['Water'] },
-  { id: 9, name: 'Blastoise', types: ['Water'] },
-  { id: 10, name: 'Caterpie', types: ['Bug'] },
-  { id: 11, name: 'Metapod', types: ['Bug'] },
-  { id: 12, name: 'Butterfree', types: ['Bug', 'Flying'] },
-  { id: 13, name: 'Weedle', types: ['Bug', 'Poison'] },
-  { id: 14, name: 'Kakuna', types: ['Bug', 'Poison'] },
-  { id: 15, name: 'Beedrill', types: ['Bug', 'Poison'] },
-  { id: 16, name: 'Pidgey', types: ['Normal', 'Flying'] },
-  { id: 17, name: 'Pidgeotto', types: ['Normal', 'Flying'] },
-  { id: 18, name: 'Pidgeot', types: ['Normal', 'Flying'] },
-  { id: 19, name: 'Rattata', types: ['Normal'] },
-  { id: 20, name: 'Raticate', types: ['Normal'] },
+  { id: 1, name: "Bulbasaur", types: ["Grass", "Poison"] },
+  { id: 2, name: "Ivysaur", types: ["Grass", "Poison"] },
+  { id: 3, name: "Venusaur", types: ["Grass", "Poison"] },
+  { id: 4, name: "Charmander", types: ["Fire"] },
+  { id: 5, name: "Charmeleon", types: ["Fire"] },
+  { id: 6, name: "Charizard", types: ["Fire", "Flying"] },
+  { id: 7, name: "Squirtle", types: ["Water"] },
+  { id: 8, name: "Wartortle", types: ["Water"] },
+  { id: 9, name: "Blastoise", types: ["Water"] },
+  { id: 10, name: "Caterpie", types: ["Bug"] },
+  { id: 11, name: "Metapod", types: ["Bug"] },
+  { id: 12, name: "Butterfree", types: ["Bug", "Flying"] },
+  { id: 13, name: "Weedle", types: ["Bug", "Poison"] },
+  { id: 14, name: "Kakuna", types: ["Bug", "Poison"] },
+  { id: 15, name: "Beedrill", types: ["Bug", "Poison"] },
+  { id: 16, name: "Pidgey", types: ["Normal", "Flying"] },
+  { id: 17, name: "Pidgeotto", types: ["Normal", "Flying"] },
+  { id: 18, name: "Pidgeot", types: ["Normal", "Flying"] },
+  { id: 19, name: "Rattata", types: ["Normal"] },
+  { id: 20, name: "Raticate", types: ["Normal"] },
 ];
 
 const sortPokemons = function logSortedPokemons(sortType) {
-  const sortedPokemons = [];
-  // Add your code here
+  const sortedPokemons = [...pokemons]; // copy the og arr
+
+  switch (sortType) {
+    case `numerically, in ascending order`:
+      // We are passing in a comparator func to specify **how** we sort
+      sortedPokemons.sort((a, b) => a - b); // a-b is an **implicit** return
+      break;
+    case `numerically, in descending order`:
+      sortedPokemons.sort((a, b) => b - a).reverse();
+      break;
+    case `alphabetically, in ascending order`:
+      break;
+    case `numerically, in ascending order`:
+      break;
+  }
 
   return sortedPokemons;
 };
 
-console.log(sortPokemons('numerically, in ascending order'));
-console.log(sortPokemons('numerically, in descending order'));
-console.log(sortPokemons('alphabetically, in ascending order'));
-console.log(sortPokemons('alphabetically, in descending order'));
+//console.log(sortPokemons("numerically, in ascending order"));
+console.log(sortPokemons("numerically, in descending order"));
+//console.log(sortPokemons("alphabetically, in ascending order"));
+//console.log(sortPokemons("alphabetically, in descending order"));
