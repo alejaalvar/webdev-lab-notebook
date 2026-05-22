2e4c3cc3771aa6094f2526279670e5da3ec4a318
Alejandro Alvarado <alejalva@pdx.edu>
2026-05-22 15:21:37 -0700

    add: query to the api for bulbasaur

--- 06-javascript/04-fetch.js
+++ 06-javascript/04-fetch.js
@@ -1,22 +1,53 @@
 const pokemonColors = {
-  normal: '#A8A77A',
-  fire: '#EE8130',
-  water: '#6390F0',
-  electric: '#F7D02C',
-  grass: '#7AC74C',
-  ice: '#96D9D6',
-  fighting: '#C22E28',
-  poison: '#ea7ce8',
-  ground: '#E2BF65',
-  flying: '#A98FF3',
-  psychic: '#F95587',
-  bug: '#A6B91A',
-  rock: '#B6A136',
-  ghost: '#735797',
-  dragon: '#6F35FC',
-  dark: '#705746',
-  steel: '#B7B7CE',
-  fairy: '#D685AD',
+  normal: "#A8A77A",
+  fire: "#EE8130",
+  water: "#6390F0",
+  electric: "#F7D02C",
+  grass: "#7AC74C",
+  ice: "#96D9D6",
+  fighting: "#C22E28",
+  poison: "#ea7ce8",
+  ground: "#E2BF65",
+  flying: "#A98FF3",
+  psychic: "#F95587",
+  bug: "#A6B91A",
+  rock: "#B6A136",
+  ghost: "#735797",
+  dragon: "#6F35FC",
+  dark: "#705746",
+  steel: "#B7B7CE",
+  fairy: "#D685AD",
 };
 
-// Add your code here
+const createNewElement = function (data) {
+  const card = document.createElement("div");
+  const h2 = document.createElement("h2");
+  const img = document.createElement("img");
+  const typesDiv = document.createElement("div");
+
+  h2.textContent = data.name;
+
+  card.append(h2, img, typesDiv);
+  return card;
+};
+
+const fetchData = async function () {
+  const url = "https://pokeapi.co/api/v2/pokemon/bulbasaur";
+
+  const pokeList = document.querySelector(".poke-list");
+
+  try {
+    const response = await fetch(url);
+    const bodyData = await response.json();
+
+    console.log(bodyData);
+    const elem = createNewElement(bodyData);
+    pokeList.append(elem);
+  } catch (error) {
+    console.error("Error fetch data form the PokeAPI", error);
+  } finally {
+    console.log("executes either way");
+  }
+};
+
+fetchData();
