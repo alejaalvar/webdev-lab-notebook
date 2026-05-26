cd53cbe49136f4c3ddd6af55493bd9178c6624f5
Alejandro Alvarado <alejalva@pdx.edu>
2026-05-26 11:44:52 -0700

    add: explantory comments for better understanding of how the program is working

--- 09-nodejs/app.js
+++ 09-nodejs/app.js
@@ -1,33 +1,35 @@
-const http = require('http');
-const static = require('node-static');
-const querystring = require('node:querystring');
+const http = require("http");
+const static = require("node-static");
+const querystring = require("node:querystring");
 
-const port = process.env.PORT || 5002;
+const port = process.env.PORT || 5002; // default to port 5002
 
-const file = new static.Server('./exercise');
+const file = new static.Server("./exercise");
 
+// Server setup
 const server = http.createServer((req, res) => {
   // main route
-  if (req.method === 'GET' && req.url === '/') {
-    file.serveFile('/welcome.html', 200, {}, req, res);
+  if (req.method === "GET" && req.url === "/") {
+    file.serveFile("/welcome.html", 200, {}, req, res);
   }
   // form route
-  else if (req.method === 'GET' && req.url === '') {
+  else if (req.method === "GET" && req.url === "") {
     // fill out this route
   }
   // form submission
-  else if (req.method === 'POST' && req.url === '') {
-    let body = '';
+  else if (req.method === "POST" && req.url === "") {
+    let body = ""; // we need mutability since we receive data from the body
 
-    req.on('data', (chunk) => {
+    req.on("data", (chunk) => {
       body += chunk;
     });
 
-    req.on('end', () => {
-      const userdata = querystring.parse(body);
-      const { usernameInput: name, emailInput: email } = userdata;
+    req.on("end", () => {
+      const userdata = querystring.parse(body); // get the key-value pairs
+      const { usernameInput: name, emailInput: email } = userdata; // destructure the key-value pairs
 
-      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.writeHead(200, { "Content-Type": "text/html" });
+      // Actually update the page once we're all done processing the submission
       res.write(`<p>Thank you for submitting your information: </p>`);
       res.write(`<p>Name: ${name}</p>`);
       res.write(`<p>Email: ${email}</p>`);
@@ -36,6 +38,7 @@ const server = http.createServer((req, res) => {
   }
 });
 
+// Start up the server for listening
 server.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
